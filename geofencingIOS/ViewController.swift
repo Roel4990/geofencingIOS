1342badacae4f5667ea3ebeefdc02fbb89fd5a96
roel <roel@contigo.im>
2023-10-02 12:20:04 +0900

    Initial Commit

---
+++ geofencingIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  geofencingIOS
+//
+//  Created by Roel on 2023/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
